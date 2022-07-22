7cb5bec85f7da62ac632f200a3ca823e46a31ce3
JSHan94 <saw1515@postech.ac.kr>
2022-07-22 23:57:34 +0900

    finish by 9-2

--- src/9-2Closures.rs
+++ src/9-2Closures.rs
@@ -24,21 +24,23 @@ fn main() {
     consume();
 
     let haystack = vec![1,2,3];
-    let contains = move |needle| haystack.contains(needle);
+    let contains = move |needle| haystack.contains(needle); // 'move ||' forces closure to take ownership of captured variables
     println!("{}", contains(&4));
     println!("{}", contains(&3));
 
     // Closure as input parameter
     // when taking a closure as an input parameter, closure's complete type must be annotated using trait.
+    // compiler implicitly create a new anonymous structure to store the captured variables inside,
+    // meanwhile implementing the functionality via one of traits
     // Fn : by reference (&T)
     // FnMut : by mutable reference (&mut T)
     // FnOnce : by value (T)
     fn apply<F>(f: F) where F: FnOnce(){
         f();
     }
-    let mut farewell = "goodbye".to_owned();
+    let mut farewell = "farewell with ".to_owned();
     let diary = || {
-        farewell.push_str("!!");
+        farewell.push_str("closure");
         println!("{}",farewell);
         mem::drop(farewell);
     };
@@ -50,4 +52,57 @@ fn main() {
     let double = |x| x*2;
     println!("{}", apply_to_3(double));
 
+
+    // function can be arguments too
+    fn apply_to_function (){
+        let mut farewell = "farewell with ".to_owned();
+        farewell.push_str("function");
+        println!("{}", farewell);
+        mem::drop(farewell);
+    }
+    apply(apply_to_function);
+
+    // returning closure as output is possible too
+    fn create_fn()-> impl Fn(){
+        let text = "Fn".to_owned();
+        move || println!("This is a: {}",text)
+    }
+
+    fn create_fnmut()-> impl FnMut(){
+        let text = "FnMut".to_owned();
+        move || println!("This is a: {}",text)
+    }
+
+    fn create_fnonce()-> impl FnOnce(){
+        let text = "FnOnce".to_owned();
+        move || println!("This is a: {}",text)
+    }
+
+    let fn_plain = create_fn();
+    let mut fn_mut = create_fnmut();
+    let fn_once = create_fnonce();
+    fn_plain();
+    fn_mut();
+    fn_once();
+
+    // iterator::any
+    let vec1 = vec![1,2,3];
+    let vec2 = vec![4,5,6];
+    let array1 = [1,2,3];
+    let array2 = [4,5,6];
+
+    println!("is 2 in vec1[1]? : {}", vec1.clone().iter().any(|&x| x == 2 )); // iter() yields &i32, Destructure to i32
+    println!("is 2 in vec1[1]? : {}", vec2.clone().into_iter().any(|x| x== 2)); // into_iter() yields i32, no Destructure
+    println!("is 2 in array1[1]? : {}", array1.clone().iter().any(|&x| x == 2 )); 
+    println!("is 2 in array2[1]? : {}", array2.clone().into_iter().any(|&x| x == 2 )); // array.into_iter() yields &i32
+    
+    // iterator::find
+    println!("find 2 in vec1: {:?}", vec1.iter().find(|&&x| x==2));
+    println!("find 2 in vec2: {:?}", vec2.into_iter().find(|&x| x==2));
+    println!("find 2 in array1: {:?}", array1.iter().find(|&&x| x == 2 ));
+    println!("find 2 in array2: {:?}", array2.iter().find(|&&x| x == 2 ));
+
+    //iterator::position
+    let index_of_two = vec1.iter().position(|x| x == &2);
+    assert_eq!(index_of_two, Some(1)); 
 }
