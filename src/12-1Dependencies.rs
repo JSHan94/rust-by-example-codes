36454b2b3613d7d3f4fe8eedd0cda67c1a74cd81
JSHan94 <saw1515@postech.ac.kr>
2022-07-26 15:14:51 +0900

    finish by 12-1

--- src/12-1Dependencies.rs
+++ src/12-1Dependencies.rs
@@ -1,3 +1,23 @@
-fn main() {
+// Rust manages dependencies with Cargo
 
+fn main() {
+    // Create Rust Project
+    // $ cargo new foo // binary
+    // $ cargo new --lib foo // library
+
+    // foo
+    // ├── Cargo.toml // config file for cargo
+    // └── src
+    //     └── main.rs
+
+    // cargo supports dependencies like below
+    // [package]
+    // name = "foo"
+    // version = "0.1.0"
+    // authors = ["mark"]
+
+    // [dependencies]
+    // clap = "2.27.1" # from crates.io
+    // rand = { git = "https://github.com/rust-lang-nursery/rand" } # from online repo
+    // bar = { path = "../bar" } # from a path in the local filesystem
 }
